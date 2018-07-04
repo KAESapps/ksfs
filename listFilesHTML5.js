887939e0cc3b31692515c56b24c6ff193d5be6b2
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2018-07-04 20:32:45 +0200

    fix listFilesHTML5

--- listFilesHTML5.js
+++ listFilesHTML5.js
@@ -1,9 +1,21 @@
 module.exports = dirEntry => {
   const dirReader = dirEntry.createReader()
-  return new Promise((resolve, reject) =>
-    dirReader.readEntries(
-      fileEntries => resolve(fileEntries.map(f => f.name)),
-      reject
-    )
-  )
+  return new Promise((resolve, reject) => {
+    let fullList = []
+
+    // read entries retrieve only a number of results
+    // call recursively to get all results
+    const readNextEntries = () =>
+      dirReader.readEntries(fileEntries => {
+        if (fileEntries.length === 0) {
+          resolve(fullList)
+        } else {
+          fullList = fullList.concat(fileEntries.map(f => f.name))
+          readNextEntries()
+        }
+      }, reject)
+
+    // start reading entries
+    readNextEntries()
+  })
 }
