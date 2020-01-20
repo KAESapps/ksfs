838b0ce51f5c7737257d596a80ff48c63edea2db
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2020-01-20 17:15:09 +0100

    openPdfBrowser rewrite

--- openPdfBrowser.js
+++ openPdfBrowser.js
@@ -1,14 +1,5 @@
 const sanitize = require("sanitize-filename")
 
-const openWindow = () => {
-  // we have to open the window immediately and store the reference
-  // otherwise popup blockers will stop us
-  let win = window.open("", "_blank")
-  if (win === null) {
-    throw new Error("Open PDF in new window blocked by browser")
-  }
-  return win
-}
 
 module.exports = args => {
   const { name, data } = args
@@ -16,11 +7,13 @@ module.exports = args => {
 
   return new Promise((resolve, reject) => {
     try {
-      win = openWindow()
+      let win = window.open("", "_blank")
+      if (win === null) {
+        throw new Error("Open PDF in new window blocked by browser")
+      }
       let urlCreator = window.URL || window.webkitURL
       let url = urlCreator.createObjectURL(blob)
       win.location.href = url
-      //TODO: filename
       resolve()
     } catch (e) {
       win.close()
