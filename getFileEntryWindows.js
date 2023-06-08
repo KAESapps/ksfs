594b6f8bfac6e0ac4b1b8c75441c3787de238294
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2023-06-08 21:23:08 +0200

    getFileEntryWindows update

--- getFileEntryWindows.js
+++ getFileEntryWindows.js
@@ -1,46 +1,47 @@
 const electronRequire = require("./utils/electronRequire")
 const fs = electronRequire("fs")
 const path = electronRequire("path")
-const mkdirp = electronRequire("mkdirp")
 
 const electron = electronRequire("electron")
+const ipcRenderer = electron.ipcRenderer
 
-const fsRoots = fsName => {
+const fsRoots = (fsName) => {
   if (fsName === "external") {
-    return "D:\\"
+    return Promise.resolve("D:\\")
   } else
-    return electron.remote.app.getPath(
+    return ipcRenderer.invoke(
+      "getPath",
       fsName === "privateAppData" || fsName === "publicAppData"
         ? "userData"
         : fsName
     )
 }
 
-const createDir = dir =>
-  new Promise(function(resolve, reject) {
+const createDir = (dir) =>
+  new Promise(function (resolve, reject) {
     if (!dir) return resolve()
-    mkdirp(dir, function(err) {
+    fs.mkdir(dir, { recursive: true }, function (err) {
       if (err) return reject(err)
       resolve()
     })
   })
 
-const fileExists = path =>
-  new Promise(resolve =>
-    fs.access(
-      path,
-      fs.constants.F_OK,
-      err => (err ? resolve(false) : resolve(true))
+const fileExists = (path) =>
+  new Promise((resolve) =>
+    fs.access(path, fs.constants.F_OK, (err) =>
+      err ? resolve(false) : resolve(true)
     )
   )
 
 module.exports = (fsRootName, filePath, opts = {}) => {
   const { create = false, dir = false } = opts
 
-  const fullPath = path.join(fsRoots(fsRootName), filePath)
-  const dirPath = dir ? fullPath : path.dirname(fullPath)
+  return fsRoots(fsRootName).then((fsRootPath) => {
+    const fullPath = path.join(fsRootPath, filePath)
+    const dirPath = dir ? fullPath : path.dirname(fullPath)
 
-  return create
-    ? createDir(dirPath).then(() => fullPath)
-    : fileExists(fullPath).then(exists => (exists ? fullPath : null))
+    return create
+      ? createDir(dirPath).then(() => fullPath)
+      : fileExists(fullPath).then((exists) => (exists ? fullPath : null))
+  })
 }
